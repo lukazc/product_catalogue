5796edf3b6c8331ab3011d58ebf2e7b7cc4e8a2a
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2024-11-22 15:44:24 +0100

    Implement search functionality in header and update product catalog layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { ProductStateService } from '../../state/product-state.service';
 
 @Component({
     selector: 'app-header',
-    imports: [MatToolbarModule],
+    imports: [MatToolbarModule, SearchBarComponent],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+    constructor(private productStateService: ProductStateService) {}
 
+    onSearch(searchTerm: string) {
+        this.productStateService.setSearchFilter(searchTerm);
+    }
 }
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [MatInputModule, MatIconModule, FormsModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.scss'
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
+  @Output() search = new EventEmitter<string>();
+  searchTerm: string = '';
+  private searchSubject = new Subject<string>();
 
+  constructor() {
+    this.searchSubject.pipe(debounceTime(300)).subscribe(searchTerm => {
+      this.search.emit(searchTerm);
+    });
+  }
+
+  onSearch() {
+    this.searchSubject.next(this.searchTerm);
+  }
 }
