a1b1f8038a88d4e078873afe61ee5c5ee6751f3e
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2024-11-22 10:52:02 +0100

    Add header component and update app layout with responsive styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, HeaderComponent],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'product_catalogue';
+export class AppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {}
 }
