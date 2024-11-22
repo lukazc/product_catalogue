f2f7d8ddbf4cc9b46b90487c903039a92c92500d
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2024-11-22 18:46:43 +0100

    Implement price filter component with reactive form and slider for price range selection

--- src/app/components/price-filter/price-filter.component.ts
+++ src/app/components/price-filter/price-filter.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { debounceTime } from 'rxjs/operators';
+import { ProductStateService } from '../../state/product-state.service';
 
 @Component({
   selector: 'app-price-filter',
-  imports: [],
   templateUrl: './price-filter.component.html',
-  styleUrl: './price-filter.component.scss'
+  styleUrls: ['./price-filter.component.scss'],
+  imports: [MatSliderModule, ReactiveFormsModule]
 })
-export class PriceFilterComponent {
+export class PriceFilterComponent implements OnInit {
+  priceForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private productStateService: ProductStateService) {
+    this.priceForm = this.fb.group({
+      priceMin: [0],
+      priceMax: [40000]
+    });
+  }
+
+  ngOnInit(): void {
+    this.priceForm.valueChanges.pipe(
+      debounceTime(300)
+    ).subscribe(values => {
+      this.productStateService.setFilterParams({
+        priceMin: values.priceMin,
+        priceMax: values.priceMax
+      });
+    });
+  }
+
+  formatLabel(value: number): string {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return `${value}`;
+  }
 }
