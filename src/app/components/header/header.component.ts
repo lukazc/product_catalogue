0e27cc837ac89460af0aa943b86262e5c6ba9dd1
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2024-11-23 21:54:03 +0100

    Add cart button with quantity badge to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,32 @@
-import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { SearchBarComponent } from '../search-bar/search-bar.component';
-import { ProductStateService } from '../../state/product-state.service';
-import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatBadge } from '@angular/material/badge';
+import { MatRipple } from '@angular/material/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { map, Observable } from 'rxjs';
+import { CartStateService } from '../../state/cart-state.service';
+import { ProductStateService } from '../../state/product-state.service';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
     selector: 'app-header',
-    imports: [MatToolbarModule, SearchBarComponent, CommonModule],
+    imports: [MatToolbarModule, SearchBarComponent, CommonModule, MatBadge, MatIcon, MatRipple],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
     currentSearchQuery$: Observable<string>;
+    totalProductsInCart$: Observable<number>;
 
-    constructor(private productStateService: ProductStateService) {
+    constructor(
+        private productStateService: ProductStateService,
+        private cartStateService: CartStateService
+    ) {
         this.currentSearchQuery$ = this.productStateService.currentSearchQuery$;
+        this.totalProductsInCart$ = this.cartStateService.cart$.pipe(
+            map(cart => cart ? cart.products.reduce((total, item) => total + item.quantity, 0) : 0)
+        );
     }
 
     onSearch(searchTerm: string) {
