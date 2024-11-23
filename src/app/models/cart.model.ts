342457c82f68c2db3910eec4095863e793f90ba1
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2024-11-23 20:07:35 +0100

    Refactor cart model to introduce LocalCartItem and update LocalCart structure

--- src/app/models/cart.model.ts
+++ src/app/models/cart.model.ts
@@ -1,29 +1,15 @@
-export interface CartItem {
-    id: number;
+export interface LocalCart {
+    userId: number;
+    products: LocalCartItem[];
+    totalPrice: number;
+    totalProducts: number;
+    totalQuantity: number;
+}
+
+export interface LocalCartItem {
     productId: number;
     quantity: number;
     title: string;
     price: number;
-    total: number;
-    discountPercentage: number;
-    discountedTotal: number;
     thumbnail: string;
-}
-
-export interface Cart {
-    id: number;
-    userId: number;
-    products: CartItem[];
-    total: number;
-    discountedTotal: number;
-    totalProducts: number;
-    totalQuantity: number;
-}
-
-export interface LocalCart {
-    userId: number;
-    products: CartItem[];
-    totalPrice: number;
-    totalProducts: number;
-    totalQuantity: number;
 }
